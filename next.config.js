a3aa07b45f991521185befe4ca4253914f76b90c
Morley Zhi <morley@springtactics.com>
2018-04-29 14:32:46 -0400

    Ignore .next, add export map

--- next.config.js
+++ next.config.js
@@ -11,4 +11,15 @@ module.exports = {
     // Important: return the modified config
     return config;
   },
+  exportPathMap: function(defaultPathMap) {
+    return {
+      "/": { page: "/" },
+      "/cn": { page: "/cn" },
+      "/de": { page: "/de" },
+      "/fr": { page: "/fr" },
+      "/en": { page: "/en" },
+      "/es": { page: "/es" },
+      "/jp": { page: "/jp" },
+    };
+  },
 };
